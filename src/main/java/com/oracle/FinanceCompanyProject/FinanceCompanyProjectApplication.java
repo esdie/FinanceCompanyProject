b3ca3c5f4b680eff593916d773201a389252a701
Shreeyash Dixit <Shreeyash Dixit@shreeyash>
2023-10-03 11:48:05 +0530

    First Commit.

---
+++ src/main/java/com/oracle/FinanceCompanyProject/FinanceCompanyProjectApplication.java
@@ -0,0 +1,14 @@
+package com.oracle.FinanceCompanyProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceCompanyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceCompanyProjectApplication.class, args);
+		System.out.println("Hii");
+	}
+
+}
---
+++ src/test/java/com/oracle/FinanceCompanyProject/FinanceCompanyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oracle.FinanceCompanyProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceCompanyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
