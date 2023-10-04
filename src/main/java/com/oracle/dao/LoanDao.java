abdd47049b6b3b231568064ac5b7c20bc6d3a0b3
sri nandu paladi <sri nandu paladi@SRINANDU>
2023-10-04 09:01:20 +0530

    nandu--yo

--- src/main/java/com/oracle/dao/LoanDao.java
+++ src/main/java/com/oracle/dao/LoanDao.java
@@ -1,15 +1,19 @@
 package com.oracle.dao;
 
 import java.sql.Date;
+import java.util.List;
 import java.util.Optional;
 
 import com.oracle.entity.Customer;
+import com.oracle.entity.LoanApplication;
 
 public interface LoanDao {
 	
 	Optional<Customer> findByLoanId(String id);
-	Optional<Customer> findByLoanType(String LoanType);
-	Optional<Customer> findByDate(Date date);
-
+	Optional<LoanApplication> findByLoanType(String LoanType);
+	Optional<LoanApplication> findByDate(Date date);
+	 Optional<LoanApplication> searchLoanApplicationByNumber(String loan_application_number);
+	 Optional<LoanApplication> searchLoanApplicationByType(int type_code);
+	 public LoanApplication applyLoan(LoanApplication la);
 
 }
